460be02f275baeb8978ecbf9283cfe06b2f9098c
jeissoni_22 <jeissoni_22@hotmail.com>
2022-07-28 21:11:55 -0500

    change libs

--- script/Bingo.sol
+++ script/Bingo.sol
@@ -5,7 +5,8 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@chainlink/contracts/RandomNumberConsumerV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract Bingo {
 
